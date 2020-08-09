c5d714160f96f4a2a8105df8c4708a1d79b75ed9
vladhuk <vladhookovskiy@gmail.com>
2020-08-10 01:10:13 +0300

    Set up prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/eslint-recommended', // Disable base rules
+    'plugin:@typescript-eslint/recommended', // Enable ts rules
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+
+  plugins: ['prettier', '@typescript-eslint'],
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+
+  rules: {
+    'prettier/prettier': [WARN],
+
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/order': WARN,
+
+    'react/jsx-filename-extension': OFF,
+  },
+};
