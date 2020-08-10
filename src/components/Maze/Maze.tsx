728df53459db0fb79f94c1e643f98950a6364595
vladhuk <vladhookovskiy@gmail.com>
2020-08-10 17:04:16 +0300

    Issue mazio-web#1: Create maze editor - Add wall to maze;

--- src/components/Maze/Maze.tsx
+++ src/components/Maze/Maze.tsx
@@ -3,6 +3,8 @@
 import React, { FunctionComponent } from 'react';
 import { Row, Container } from 'react-bootstrap';
 import Cell from './Cell';
+import Wall from './Wall';
+import { WallPosition } from './Wall/Wall';
 
 interface Props {
   size: {
@@ -12,21 +14,38 @@ interface Props {
 }
 
 const Maze: FunctionComponent<Props> = ({ size }: Props) => {
+  const horizontalArr = Array(size.height).fill(null);
+  const verticalArr = Array(size.width).fill(null);
+
   return (
-    <Container>
-      {Array(size.height)
-        .fill(null)
-        .map((val1, i1) => {
-          return (
-            <Row key={i1}>
-              {Array(size.width)
-                .fill(null)
-                .map((val2, i2) => {
-                  return <Cell key={i2} />;
-                })}
-            </Row>
-          );
+    <Container className="maze">
+      <Row>
+        {horizontalArr.map(() => {
+          return <Wall position={WallPosition.HORIZONTAL} />;
         })}
+      </Row>
+      {verticalArr.map(() => {
+        return (
+          <>
+            <Row>
+              <Wall position={WallPosition.VERTICAL} />
+              {horizontalArr.map(() => {
+                return (
+                  <>
+                    <Cell />
+                    <Wall position={WallPosition.VERTICAL} />
+                  </>
+                );
+              })}
+            </Row>
+            <Row>
+              {horizontalArr.map(() => {
+                return <Wall position={WallPosition.HORIZONTAL} />;
+              })}
+            </Row>
+          </>
+        );
+      })}
     </Container>
   );
 };
