70c39f6659baa1eaf54ba060d4a848b131e7855b
vladhuk <vladhookovskiy@gmail.com>
2020-08-18 17:05:42 +0300

    Issue mazio-web#1: Create maze editor - Implement maze building to component;

--- src/components/Maze/Maze.tsx
+++ src/components/Maze/Maze.tsx
@@ -3,57 +3,54 @@
 import React, { FunctionComponent } from 'react';
 import { Row, Container } from 'react-bootstrap';
 import Cell from './Cell';
-import Wall from './Wall';
-import { WallPosition } from './Wall/Wall';
-import Structure, { WallType } from '../../types/models/Maze/Structure';
+import Structure, {
+  WallType,
+  CellType,
+} from '../../types/models/Maze/Structure';
+import { buildWalls, buildCells } from './Maze.service';
+import Wall, { WallPosition } from './Wall';
 
 type Props = Structure;
 
 const Maze: FunctionComponent<Props> = ({ size, walls, cells }: Props) => {
-  const horizontalArr = Array(size.height).fill(null);
-  const verticalArr = Array(size.width).fill(null);
+  const wallsMatrix = buildWalls(size, walls);
+  const cellsMatrix = buildCells(size, cells);
 
-  return (
-    <Container className="maze">
-      <Row>
-        {horizontalArr.map(() => {
-          return (
-            <Wall position={WallPosition.HORIZONTAL} type={WallType.EXTERNAL} />
-          );
-        })}
-      </Row>
-      {verticalArr.map(() => {
-        return (
-          <>
-            <Row>
-              <Wall position={WallPosition.VERTICAL} type={WallType.OUTPUT} />
-              {horizontalArr.map(() => {
-                return (
-                  <>
-                    <Cell />
-                    <Wall
-                      position={WallPosition.VERTICAL}
-                      type={WallType.RUBBER}
-                    />
-                  </>
-                );
-              })}
-            </Row>
-            <Row>
-              {horizontalArr.map(() => {
-                return (
-                  <Wall
-                    position={WallPosition.HORIZONTAL}
-                    type={WallType.TRANSLUCENT}
-                  />
-                );
-              })}
-            </Row>
-          </>
-        );
-      })}
-    </Container>
-  );
+  const getRowElements = (
+    wallsRow: WallType[],
+    cellsRow: CellType[]
+  ): JSX.Element => {
+    const elements = [];
+    for (let x = 0; x < cellsRow.length; x += 1) {
+      elements.push(
+        <Wall position={WallPosition.VERTICAL} type={wallsRow[x]} />
+      );
+      elements.push(<Cell type={cellsRow[x]} />);
+    }
+    elements.push(
+      <Wall position={WallPosition.VERTICAL} type={wallsRow[cellsRow.length]} />
+    );
+
+    return <>{elements}</>;
+  };
+
+  const getWallsRow = (wallsRow: WallType[]): JSX.Element[] => {
+    return wallsRow.map((wall, x) => (
+      <Wall position={WallPosition.HORIZONTAL} type={wallsRow[x]} />
+    ));
+  };
+
+  const rows = [];
+
+  for (let y = 0; y < size.height; y += 1) {
+    rows.push(<Row>{getWallsRow(wallsMatrix[y * 2])}</Row>);
+    rows.push(
+      <Row>{getRowElements(wallsMatrix[y * 2 + 1], cellsMatrix[y])}</Row>
+    );
+  }
+  rows.push(<Row>{getWallsRow(wallsMatrix[wallsMatrix.length - 1])}</Row>);
+
+  return <Container className="maze">{rows}</Container>;
 };
 
 export default Maze;
