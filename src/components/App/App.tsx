d5b35ef657fef83ff5d951ce7e52e890c85abac9
vladhuk <vladhookovskiy@gmail.com>
2020-08-18 17:05:59 +0300

    Issue mazio-web#1: Create maze editor - Add test data;

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,22 @@
 import React, { FunctionComponent } from 'react';
 import Maze from '../Maze';
+import {
+  Wall,
+  WallType,
+  Cell,
+  CellType,
+} from '../../types/models/Maze/Structure';
 
 const App: FunctionComponent = () => {
-  return <Maze size={{ height: 5, width: 5 }} walls={[]} cells={[]} />;
+  const walls: Wall[] = [
+    { location: { x: 4, y: 0 }, type: WallType.OUTPUT },
+    { location: { x: 4, y: 5 }, type: WallType.OUTPUT },
+    { location: { x: 3, y: 3 }, type: WallType.STONE },
+  ];
+
+  const cells: Cell[] = [{ location: { x: 3, y: 0 }, type: CellType.HOSPITAL }];
+
+  return <Maze size={{ height: 5, width: 5 }} walls={walls} cells={cells} />;
 };
 
 export default App;
