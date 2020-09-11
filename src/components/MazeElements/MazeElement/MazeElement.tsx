9329102894682b707c0819671409773ac369e456
vladhuk <vladhookovskiy@gmail.com>
2020-09-11 12:52:21 +0300

    Issue mazio-web#1: Create maze editor - Clean up;

--- src/components/MazeElements/MazeElement/MazeElement.tsx
+++ src/components/MazeElements/MazeElement/MazeElement.tsx
@@ -1,37 +1,33 @@
-import React, { useState } from 'react';
+import React, { forwardRef, useState } from 'react';
 import './MazeElement.scss';
 
 interface Props {
   className?: string;
 }
 
-const MazeElement = React.forwardRef<HTMLDivElement, Props>(
-  ({ className }, ref) => {
-    const [isHover, setHover] = useState(false);
-    const [isFocus, setFocus] = useState(false);
+const MazeElement = forwardRef<HTMLDivElement, Props>(({ className }, ref) => {
+  const [isHover, setHover] = useState(false);
+  const [isFocus, setFocus] = useState(false);
 
-    const hoverClassName = isHover ? 'hover' : '';
-    const elementClassName = `maze-element ${hoverClassName} ${className}`;
+  const hoverClassName = isHover ? 'hover' : '';
+  const elementClassName = `maze-element ${hoverClassName} ${className}`;
 
-    const blurElement = (eventTarget: EventTarget) =>
-      (eventTarget as HTMLDivElement).blur();
+  const blurElement = (eventTarget: EventTarget) =>
+    (eventTarget as HTMLDivElement).blur();
 
-    return (
-      <div
-        ref={ref}
-        tabIndex={0}
-        className={elementClassName}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        onFocus={() => setTimeout(() => setFocus(true), 200)}
-        onBlur={() => setFocus(false)}
-        onClick={(event) => isFocus && blurElement(event.target)}
-        onKeyDown={(event) =>
-          event.key === 'Escape' && blurElement(event.target)
-        }
-      />
-    );
-  }
-);
+  return (
+    <div
+      ref={ref}
+      tabIndex={0}
+      className={elementClassName}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      onFocus={() => setTimeout(() => setFocus(true), 200)}
+      onBlur={() => setFocus(false)}
+      onClick={(event) => isFocus && blurElement(event.target)}
+      onKeyDown={(event) => event.key === 'Escape' && blurElement(event.target)}
+    />
+  );
+});
 
 export default MazeElement;
