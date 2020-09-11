3ba311f118492b57a39e16519f3cf569d8876bf4
vladhuk <vladhookovskiy@gmail.com>
2020-09-11 14:47:28 +0300

    Issue mazio-web#1: Create maze editor - Improve style;

--- src/components/MazeElementsMenu/MazeElementsMenuContainer.tsx
+++ src/components/MazeElementsMenu/MazeElementsMenuContainer.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { Col } from 'react-bootstrap';
 
 const MazeElementsMenuContainer: FunctionComponent = ({ children }) => (
-  <Col md={4} className="p-3 border rounded bg-light">
+  <Col md={4} className="p-3  h-100 border rounded bg-light">
     {children}
   </Col>
 );
