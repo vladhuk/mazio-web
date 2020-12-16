5ca9138af3ee5a67b703eeea099bffe8e3d4b828
vladhuk <vladhookovskiy@gmail.com>
2020-12-17 00:44:15 +0200

    Issue mazio-web#3: Maze editor. Add resizing - Refactor maze resizer; - Add handler on minimal value pressing;

--- src/components/MazeResizer/MazeResizer.tsx
+++ src/components/MazeResizer/MazeResizer.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, FunctionComponent } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import Size from '../../types/models/Maze/Structure/Size';
+import Size, { PartialSize } from '../../types/models/Maze/Structure/Size';
 import BlockedNumberFormControl from '../BlockedNumberFormInput';
 import FormContainer from '../FormContainer';
 
@@ -8,45 +8,41 @@ interface Props {
   minSize: Size;
   size: Size;
   setSize(size: Size): void;
+  onRestrictedSize?(restrictedSize: PartialSize): void;
 }
 
 const MazeResizer: FunctionComponent<Props> = ({
   minSize,
-  size: { width, height },
+  size,
   setSize,
-}) => (
-  <FormContainer>
-    <h4>Size</h4>
-    <Row className="justify-content-center align-items-center">
-      <Col md={4}>
-        <BlockedNumberFormControl
-          min={minSize.width}
-          required
-          value={width}
-          onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            setSize({
-              width: parseInt(event.target.value, 10),
-              height,
-            })
-          }
-        />
-      </Col>
-      <div className="font-weight-bolder">✕</div>
-      <Col md={4}>
-        <BlockedNumberFormControl
-          min={minSize.height}
-          required
-          value={height}
-          onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            setSize({
-              width,
-              height: parseInt(event.target.value, 10),
-            })
-          }
-        />
-      </Col>
-    </Row>
-  </FormContainer>
-);
+  onRestrictedSize,
+}) => {
+  const getInput: (property: keyof Size) => JSX.Element = (property) => (
+    <BlockedNumberFormControl
+      required
+      value={size[property]}
+      onChange={(event: ChangeEvent<HTMLInputElement>) => {
+        const newValue = parseInt(event.target.value, 10);
+
+        if (newValue < minSize[property]) {
+          onRestrictedSize?.({ [property]: newValue });
+        } else {
+          setSize({ ...size, [property]: newValue });
+        }
+      }}
+    />
+  );
+
+  return (
+    <FormContainer>
+      <h4>Size</h4>
+      <Row className="justify-content-center align-items-center">
+        <Col md={4}>{getInput('width')}</Col>
+        <div className="font-weight-bolder">✕</div>
+        <Col md={4}>{getInput('height')}</Col>
+      </Row>
+    </FormContainer>
+  );
+};
 
 export default MazeResizer;
