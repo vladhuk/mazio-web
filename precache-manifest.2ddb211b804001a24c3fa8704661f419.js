81ff899f293b47e49ec2e04cf9dbe226643ab18a
vladhuk <vladhookovskiy@gmail.com>
2020-12-24 15:30:48 +0200

    Updates

---
+++ precache-manifest.2ddb211b804001a24c3fa8704661f419.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ee9ca2929dfaef9805a147c4d3a555",
+    "url": "/mazio-web/index.html"
+  },
+  {
+    "revision": "6d31af0e94efed6ae137",
+    "url": "/mazio-web/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a937b39f9fab175c1afe",
+    "url": "/mazio-web/static/css/main.591f99bc.chunk.css"
+  },
+  {
+    "revision": "6d31af0e94efed6ae137",
+    "url": "/mazio-web/static/js/2.22ec8256.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/mazio-web/static/js/2.22ec8256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a937b39f9fab175c1afe",
+    "url": "/mazio-web/static/js/main.6119ad10.chunk.js"
+  },
+  {
+    "revision": "acbfdce742fa84bb7911",
+    "url": "/mazio-web/static/js/runtime-main.77b58afd.js"
+  },
+  {
+    "revision": "3cd0dd4289bb787261e396f468db2689",
+    "url": "/mazio-web/static/media/chest.3cd0dd42.svg"
+  },
+  {
+    "revision": "7af20470f85bf1ad733bfeb8d0ce3037",
+    "url": "/mazio-web/static/media/home.7af20470.svg"
+  },
+  {
+    "revision": "4756cba1dad2fbe7f46037179e46ad69",
+    "url": "/mazio-web/static/media/hospital.4756cba1.svg"
+  },
+  {
+    "revision": "f7627444e3f374002bf0d65b4aab1cf9",
+    "url": "/mazio-web/static/media/pit-in.f7627444.svg"
+  },
+  {
+    "revision": "3e2b5de74fa338556ec509bb68233257",
+    "url": "/mazio-web/static/media/pit-out.3e2b5de7.svg"
+  },
+  {
+    "revision": "8a0c49300288b7a64149a68ef32dfcea",
+    "url": "/mazio-web/static/media/river-end.8a0c4930.svg"
+  },
+  {
+    "revision": "0901edd9edb2ca734282c0981239532b",
+    "url": "/mazio-web/static/media/river-start.0901edd9.svg"
+  },
+  {
+    "revision": "fa24afaffecb7f544d5fb52e1015b507",
+    "url": "/mazio-web/static/media/river.fa24afaf.svg"
+  },
+  {
+    "revision": "07848304894b24c4ee0bf62180106852",
+    "url": "/mazio-web/static/media/target.07848304.svg"
+  },
+  {
+    "revision": "5ecd40b05f0233a9e4a0877f28c2a120",
+    "url": "/mazio-web/static/media/trap.5ecd40b0.svg"
+  }
+]);
